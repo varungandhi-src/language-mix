6ea9df7a93b18c5531b91c72c52042d53e01a20a
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-12-26 22:26:58 +0530

    Make C++ code more C++-ey

--- cpp.h
+++ cpp.h
@@ -1,5 +1,19 @@
+#include <atomic>
 namespace cpp {
     inline int multiplyBy3(int x) {
         return x * 3;
     }
+}
+
+namespace x {
+  namespace y {
+    class Z {
+      public:
+      private:
+    };
+    enum class E {
+      A
+    };
+    constexpr E e = E::A;
+  }
 }
