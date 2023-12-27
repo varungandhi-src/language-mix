956fe71b9bf2e8823153f355aa78999d272b6d5b
Varun Gandhi <varun.gandhi@sourcegraph.com>
2023-12-27 10:59:59 +0530

    Tweak mathematica file to avoid MATLAB confusion

--- mathematica.m
+++ mathematica.m
@@ -1,3 +1,7 @@
+(* Avoid misclassifying by enry for testing logo *)
+s := StringRiffle[{"a", "b", "c", "d", "e"}, ", "]
+Flatten[{{a, b}, {c, {d}, e}, {f, {g, h}}}]
+
 square[x_] := x ^ 2
 
 fourthpower[x_] := square[square[x]]
